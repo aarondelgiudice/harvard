5b89525c85d897c9150f7ea6a6d6281e192fbc39
Aaron Del Giudice <aarondelgiudice@gmail.com>
2023-10-28 12:33:19 -0400

    initial commit

---
+++ e10b/pset3/Prob1.java
@@ -0,0 +1,8 @@
+public class Prob1 {
+    public static void main(String[] vargs) {
+        String[] args = null;
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println(args[i]);
+        }
+    }
+}
